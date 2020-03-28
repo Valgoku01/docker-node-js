a2f76ec5da7b96f923d5cedc0d5c9a109fdd21fc
Valentin BIAUD <valentin.biaud@gmail.com>
2020-03-28 22:48:15 +0000

    Adding UT file

---
+++ tests/test_server.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Server', function() {
+  describe('Server', function() {
+    it('Hello world', function() {
+      assert.equal("Hello World", "Hello World");
+    });
+  });
+});
