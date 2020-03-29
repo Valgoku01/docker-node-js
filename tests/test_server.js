f545441e5cb5efbe039ac0fef4544b8d02c68450
Valentin BIAUD <valentin.biaud@gmail.com>
2020-03-29 19:12:52 +0100

    Integration React app

--- tests/test_server.js
+++ tests/test_server.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('Server', function() {
   describe('Server', function() {
     it('Hello world', function() {
-      assert.equal("Hello World", "Hello World");
+      assert.equal("Hello World !", "Hello World !");
     });
   });
 });
